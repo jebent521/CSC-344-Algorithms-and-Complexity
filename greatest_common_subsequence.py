2446a6d0f27995e1bb9842283488272d88c5ddff
Jonah Ebent <44449970+jebent521@users.noreply.github.com>
2024-11-08 16:21:07 -0500

    Reconstruct string

--- greatest_common_subsequence.py
+++ greatest_common_subsequence.py
@@ -6,21 +6,24 @@
 #   0 0 0 0 0
 
 def gcs(s1, s2):
-    memo = [[-1] * (len(s2)+1) for _ in range(len(s1)+1)]
-    print(f(memo, s1, s2, 0, 0))
+    memo = [[None] * (len(s2)+1) for _ in range(len(s1)+1)]
+    print(f(memo, s1, s2, 0, 0, ''))
     [print(*row, sep='\t') for row in memo]
 
-def f(memo, s1, s2, i, j):
-    # print(i, j)
-    # [print(row) for row in memo]
-
-    if memo[i][j] == -1:
+def f(memo, s1, s2, i, j, sub):
+    if memo[i][j] == None:
         if i == len(s1) or j == len(s2):
-            memo[i][j] = 0
+            memo[i][j] = (0, '')
         elif s1[i] == s2[j]:
-            memo[i][j] = 1 + f(memo, s1, s2, i+1, j+1)
+            next = f(memo, s1, s2, i+1, j+1, sub)
+            memo[i][j] = (1 + next[0], s1[i] + next[1])
         else:
-            memo[i][j] = max(f(memo, s1, s2, i+1, j), f(memo, s1, s2, i, j+1))
+            down = f(memo, s1, s2, i+1, j, sub)
+            right = f(memo, s1, s2, i, j+1, sub)
+            if down[0] > right[0]:
+                memo[i][j] = down
+            else:
+                memo[i][j] = right
     return memo[i][j]
 
 gcs('ace', 'abcde')
