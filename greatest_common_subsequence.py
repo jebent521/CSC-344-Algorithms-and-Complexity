9f3e533b0298d7c2d036fe2cce52e9e69f7121b6
Jonah Ebent <44449970+jebent521@users.noreply.github.com>
2024-11-08 16:26:51 -0500

    Hot diggity

--- greatest_common_subsequence.py
+++ greatest_common_subsequence.py
@@ -1,29 +1,21 @@
-#   s t r 1
-# s 0 0 0 0 0
-# t 0 0 0 0 0
-# r 0 0 0 0 0
-# 2 0 0 0 0 0
-#   0 0 0 0 0
-
+# Jonah Ebent, 11/8/24
 def gcs(s1, s2):
+    '''returns the longest common sequence of letters in the two strings'''
     memo = [[None] * (len(s2)+1) for _ in range(len(s1)+1)]
-    print(f(memo, s1, s2, 0, 0, ''))
-    [print(*row, sep='\t') for row in memo]
+    return f(memo, s1, s2, 0, 0, '')
 
 def f(memo, s1, s2, i, j, sub):
+    '''recursive helper function'''
     if memo[i][j] == None:
         if i == len(s1) or j == len(s2):
-            memo[i][j] = (0, '')
+            memo[i][j] = ''
         elif s1[i] == s2[j]:
-            next = f(memo, s1, s2, i+1, j+1, sub)
-            memo[i][j] = (1 + next[0], s1[i] + next[1])
+            memo[i][j] = s1[i] + f(memo, s1, s2, i+1, j+1, sub)
         else:
             down = f(memo, s1, s2, i+1, j, sub)
             right = f(memo, s1, s2, i, j+1, sub)
-            if down[0] > right[0]:
+            if len(down) > len(right):
                 memo[i][j] = down
             else:
                 memo[i][j] = right
     return memo[i][j]
-
-gcs('ace', 'abcde')
